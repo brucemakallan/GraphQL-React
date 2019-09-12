c32c40122548d8a1b967d3b9cc88368adb9771b3
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-09-12 21:19:49 +0300

    Add Apollo and her dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const link = createHttpLink({
+  uri: 'https://authors-haven-rails-graphql.herokuapp.com/graphql'
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
