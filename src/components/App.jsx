02dc0c59a5ed90d6752c39d1b0f6ec17708831ab
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-09-12 20:57:13 +0300

    Add material UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const App = () => (
-  <div className="App">
-      Welcome
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary" className={classes.button}>
+        Primary
+      </Button>
+    </div>
+  );
+};
 
 export default App;
